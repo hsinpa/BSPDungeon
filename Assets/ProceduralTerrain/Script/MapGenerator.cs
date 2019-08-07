7337f73826c2051c9b43278268e78b3dc15f9a6d
hsinpa <hsinpa@gmail.com>
2019-08-07 23:10:27 +0800

    Update on making noise map generator

---
+++ Assets/ProceduralTerrain/Script/MapGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GeneratNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+            mapWidth = 1;
+
+        if (mapHeight < 1)
+            mapHeight = 1;
+
+        if (lacunarity < 1)
+            lacunarity = 1;
+
+        if (octaves < 0)
+            octaves = 0;
+    }
+
+}
